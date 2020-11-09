71dde8ab54f196b9ebdf30ec4e57ad68367df569
Wajid Ahmad <wajid2665@gmail.com>
2020-11-09 20:09:16 +0100

    added sql connection

--- app.js
+++ app.js
@@ -4,9 +4,29 @@ const app = express();
 const serverPort = process.env.PORT || 4000;
 
 
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "den1.mysql5.gear.host",
+  user: "kingsman",
+  password: "dbPassword123!",
+  database: "kingsman"
+
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "INSERT INTO testtable (idtesttable, test123) VALUES ('3', 'Highway 39')";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("1 record inserted");
+    });
+  });
+
 app.get("/", (req,res) =>{
 
-    res.send("HELLO!");
+    res.send("HELLO123");
 })
 
 app.listen(serverPort,  (error) => 
