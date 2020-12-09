0b0d608f5d4da769169095a67689d077839306e9
Jarrald <novijgod@yahoo.dk>
2020-12-09 18:38:21 +0100

    create

--- routes/services.js
+++ routes/services.js
@@ -28,35 +28,6 @@ const adminService = fs.readFileSync(
   __dirname + "/../public/adminlogin/admin_service.html",
   "utf8"
 );
-
-router.get("/services", async (req, res) => {
-  return res.send(await Service.query().select());
-});
-
-router.delete("/services/", async (req, res) => {
-  const id = escape(req.body.id);
-  //Checks if it was deleted
-  if ((await Service.query().deleteById(id)) == 1)
-    return res.send("Service deleted succesfully.");
-  return res.send("Error deleting service.");
-});
-router.put("/services/", async (req, res) => {
-  const id = escape(req.body.id);
-  const name = escape(req.body.name);
-  const price = escape(req.body.price);
-  const duration = escape(req.body.duration);
-  console.log(req.body);
-  if ((await Service.query().patch({
-      name: name,
-      price: price,
-      duration: duration,
-  }).findById(id)) == 1
-  ) {
-    return res.send("Service updated succesfully.");
-  }
-    return res.send("Error deleting service.");
-});
-
 router.get("/showservices", async (req, res) => {
   //adminlogin
   if (req.session.adminTrue === true) {
@@ -66,7 +37,55 @@ router.get("/showservices", async (req, res) => {
   else if (req.session.isOn === true) {
     return res.send(UserheaderPage + showService + footerPage);
   }
-  return res.send(headerPage + showService + footerPage);
+  return res.send(headerPage + adminService + footerPage);
 });
+router.get("/services", async (req, res) => {
+  return res.send(await Service.query().select());
+});
+
+router.post("/services/", async (req, res) => {
+  console.log(req.body);
+  //const {name, price, duration, description} = req.body
+  //Checks if it was deleted
+  try{
+    return res.send(await Service.query().insertAndFetch(req.body));
+  }catch(error){
+    console.log(error)
+    return res.send("Error creating service.");
+  }
+  
+});
+router.put("/services/", async (req, res) => {
+  const id = escape(req.body.id);
+  const name = escape(req.body.name);
+  const price = escape(req.body.price);
+  const duration = escape(req.body.duration);
+  const description = escape(req.body.description);
+  console.log(req.body);
+  if ((await Service.query().patch({
+      name: name,
+      price: price,
+      duration: duration,
+      description: description
+  }).findById(id)) == 1
+  ) {
+    return res.send("Service updated succesfully.");
+  }
+    return res.send("Error deleting service.");
+});
+router.delete("/services/", async (req, res) => {
+  const id = escape(req.body.id);
+  //Checks if it was deleted
+  if ((await Service.query().deleteById(id)) == 1)
+    return res.send("Service deleted succesfully.");
+  return res.send("Error deleting service.");
+});
+router.patch("/services", async(req, res) => {
+  return "fail";
+});
+
+
+
+
 
 module.exports = router;
