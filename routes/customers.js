3689b346efbbb750c8608935340e01b54cc1df3d
Jarrald <novijgod@yahoo.dk>
2020-12-16 14:10:38 +0100

    customer name in booking crud

--- routes/customers.js
+++ routes/customers.js
@@ -4,5 +4,10 @@ const router = require('express').Router();
 router.get("/customers", async (req, res) => {
     return res.send(await Customer.query().select());
 });
+router.get("/customers/:id", async (req, res) => {
+    const id = escape(req.params.id);
+    return res.send(await Customer.query().select().where("id", id));
+})
+
 
 module.exports = router;
