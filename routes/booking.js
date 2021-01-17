2d24a67939081cdc0ad97a297346ddbc4bb1239c
Jarrald <novijgod@yahoo.dk>
2021-01-17 15:53:31 +0100

    fix price

--- routes/booking.js
+++ routes/booking.js
@@ -25,8 +25,6 @@ const mailCreds = require("../config/mailCreds");
 const Service = require('../models/Service');
 
 const userHeader = fs.readFileSync(__dirname + '/../public/userlogin/user_header.html', "utf8");
-const adminBookPage = fs.readFileSync(__dirname + '/../public/adminlogin/admin_book.html', "utf8");
-const adminHeader = fs.readFileSync(__dirname + '/../public/adminlogin/admin_header.html', "utf8");
 
 router.get("/bookings", async (req, res) => {
     if(req.session.adminTrue !== true)
